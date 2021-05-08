c9bec8f7e5c8002aaf0babb966c146b48ad64b40
Nick Bild <nick.bild@gmail.com>
2021-05-08 09:06:42 -0400

    Refactor code.

--- pong.c
+++ pong.c
@@ -4,16 +4,16 @@
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "hardware/irq.h"
-#include "blink.pio.h"
+#include "pong.pio.h"
 #include "hardware/dma.h"
 
 #define DMA_CHANNEL 0
 #define DMA_CHANNEL_H 1
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, uint pin_side);
-void horizontal_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, uint pin_side);
-void hsync_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, uint pin_side);
-void hsync_forever_initial(PIO pio, uint sm, uint offset, uint pin, uint freq, uint pin_side);
+void vertical_forever(PIO pio, uint sm, uint offset, uint pin, uint pin_side);
+void horizontal_forever(PIO pio, uint sm, uint offset, uint pin, uint pin_side);
+void hsync_forever(PIO pio, uint sm, uint offset, uint pin, uint pin_side);
+void hsync_forever_initial(PIO pio, uint sm, uint offset, uint pin, uint pin_side);
 void dma_handler();
 void dma_handler_h();
 void draw_computer_paddle(int y, int color);
@@ -23,13 +23,16 @@ void draw_ball(int x_start, int y_start, int color);
 void adjust_computer_paddle();
 void point_scored();
 
+// Video buffer.
 uint8_t src_h[224000] = {};
 
+// Ball position and physics.
 int vx = 1;
 int vy = 1;
 int ball_x = 50;
 int ball_y = 150;
 
+// Paddle posiioning.
 int computer_paddle_y = 30;
 int player_paddle_y = 30;
 const uint8_t PLAYER_UP_DOWN = 14;
@@ -38,51 +41,47 @@ const uint8_t PLAYER_UP_PIN = 15;
 int main() {
     stdio_init_all();
 
+    // GPIO setup.
     gpio_init(PLAYER_UP_PIN);
     gpio_set_dir(PLAYER_UP_PIN, GPIO_IN);
     gpio_init(PLAYER_UP_DOWN);
     gpio_set_dir(PLAYER_UP_DOWN, GPIO_IN);
 
-    // Black out all pixels
+    // Black out all pixels.
     for (int i=0; i<224000; i++) {
         src_h[i] = 0;
     }
 
-    // Top border (4 lines)
+    // Top border (4 lines).
     for (int i=0; i<2560; i++) {
         src_h[i] = 1;
     }
 
-    // Bottom border (4 lines)
+    // Bottom border (4 lines).
     for (int i=220800; i<223360; i++) {
         src_h[i] = 1;
     }
 
     // 400-1600 MHz, 1-7, 1-7
-    set_sys_clock_pll(1550000000, 6, 1); // 250 MHz, 4 ns/clock, 10 clocks/pixel
+    set_sys_clock_pll(1550000000, 6, 1);
 
+    // PIO program loading.
     PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blinky_program);
+    uint offset = pio_add_program(pio, &vertical_program);
     uint offset2 = pio_add_program(pio1, &hsync_program);
-    
     uint offset4 = pio_add_program(pio1, &horizontal_program);
-    // printf("Loaded program at %d\n", offset);
-
-
+    
+    // Start the VGA signal.
     uint sm = 0;
-    // hw_clear_bits(&pio->inte0, 1u << sm);
-    // hw_clear_bits(&pio->inte1, 1u << sm);
-
-
-
-    blink_pin_forever(pio, 0, offset, 0, 100000000, 4);
-    hsync_forever(pio1, 1, offset2, 4, 100000000, 3);
-    hsync_forever_initial(pio1, 2, offset2, 5, 100000000, 3); // Initial
-
-    horizontal_forever(pio1, 0, offset4, 1, 100000000, 3);
+    vertical_forever(pio, 0, offset, 0, 4);
+    hsync_forever(pio1, 1, offset2, 4, 3);
+    hsync_forever_initial(pio1, 2, offset2, 5, 3);
+    horizontal_forever(pio1, 0, offset4, 1, 3);
     
-    
-    
+    ////
+    // Set up PIO DMA streams.
+    ////
+
     dma_channel_config channel_config = dma_channel_get_default_config(DMA_CHANNEL);
     channel_config_set_transfer_data_size(&channel_config, DMA_SIZE_32);
     channel_config_set_read_increment(&channel_config, true);
@@ -100,10 +99,6 @@ int main() {
     irq_set_enabled(DMA_IRQ_0, true);
     dma_handler();
 
-
-
-
-    
     dma_channel_config channel_config_h = dma_channel_get_default_config(DMA_CHANNEL_H);
     channel_config_set_transfer_data_size(&channel_config_h, DMA_SIZE_8);
     channel_config_set_read_increment(&channel_config_h, true);
@@ -121,12 +116,13 @@ int main() {
     irq_set_enabled(DMA_IRQ_1, true);
     dma_handler_h();
 
-
+    // Initial playfield.
     draw_computer_paddle(computer_paddle_y, 1);
     draw_player_paddle(1);
     draw_net();
     sleep_ms(25000);
 
+    // Game loop.
     while(true) {
         // Move ball.
         draw_ball(ball_x, ball_y, 0);
@@ -166,6 +162,7 @@ int main() {
         draw_net();
         adjust_computer_paddle();
 
+        // Controller input.
         if (gpio_get(PLAYER_UP_PIN) == 0) {
             draw_player_paddle(0);
             player_paddle_y--;
@@ -269,28 +266,20 @@ void dma_handler() {
     static uint32_t src[] = {349, 7750, 1295473, 15999, 80750};
     dma_hw->ints0 = 1u << DMA_CHANNEL;
     dma_channel_set_read_addr(DMA_CHANNEL, &src[0], true);
-} // 1039220
+}
 
 void dma_handler_h() {
-    // Not showing: 10 on left, 10 on right.
-    // 25.6 - 25.42 = 0.18 microseconds too long = 180 ns = 4.5 pixels
-    // static uint8_t src_h[] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1};
-    // static uint8_t src_h[] = {1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0};    
-
     dma_hw->ints0 = 1u << DMA_CHANNEL_H;
     dma_channel_set_read_addr(DMA_CHANNEL_H, &src_h[0], true);
 }
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, uint pin_side) {
-    blink_program_init(pio, sm, offset, pin, pin_side);
+void vertical_forever(PIO pio, uint sm, uint offset, uint pin, uint pin_side) {
+    vertical_program_init(pio, sm, offset, pin, pin_side);
     pio_sm_set_enabled(pio, sm, true);
     pio->txf[sm] = 7994;
-
-    // printf("Blinking pin %d at %d Hz\n", pin, freq);
-    // pio->txf[sm] = clock_get_hz(clk_sys) / 2 * freq; // 312,500,000
 }
 
-void horizontal_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, uint pin_side) {
+void horizontal_forever(PIO pio, uint sm, uint offset, uint pin, uint pin_side) {
     horizontal_program_init(pio, sm, offset, pin, pin_side);
     
     pio->sm[sm].shiftctrl = (8u << PIO_SM0_SHIFTCTRL_PULL_THRESH_LSB);
@@ -299,13 +288,13 @@ void horizontal_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, uint
     pio->txf[sm] = 639;
 }
 
-void hsync_forever(PIO pio, uint sm, uint offset, uint pin, uint freq, uint pin_side) {
+void hsync_forever(PIO pio, uint sm, uint offset, uint pin, uint pin_side) {
     hsync_program_init(pio, sm, offset, pin, pin_side);
     pio_sm_set_enabled(pio, sm, true);
     pio->txf[sm] = 6809;
 }
 
-void hsync_forever_initial(PIO pio, uint sm, uint offset, uint pin, uint freq, uint pin_side) {
+void hsync_forever_initial(PIO pio, uint sm, uint offset, uint pin, uint pin_side) {
     hsync_program_init(pio, sm, offset, pin, pin_side);
     pio_sm_set_enabled(pio, sm, true);
     pio->txf[sm] = 6554;
